5ddf41a9700ea707e4ea5feb130c626fe4a20d94
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-09-28 16:20:56 +0200

    feat: add a hacky home and away key

--- app.py
+++ app.py
@@ -1,73 +1,73 @@
+import os
+
 from flask import Flask, jsonify, render_template
 from flask_socketio import SocketIO
 
-import os
-
 app = Flask(__name__)
-sio = SocketIO(app, cors_allowed_origins='*')
+sio = SocketIO(app, cors_allowed_origins="*")
 
 # Same structure as the WiseHockey API for easy integration
 tally = {
-    "homeTeamStatistics": {
-        "totalStatistics": {
-            "shotStatistics": {
-                "shotsOnGoal": 0
-            }}},
-    "awayTeamStatistics": {
-        "totalStatistics": {
-            "shotStatistics": {
-                "shotsOnGoal": 0
-            }}}
+    "home": 0,
+    "away": 0,
+    "homeTeamStatistics": {"totalStatistics": {"shotStatistics": {"shotsOnGoal": 0}}},
+    "awayTeamStatistics": {"totalStatistics": {"shotStatistics": {"shotsOnGoal": 0}}},
 }
 
 
 def modify_tally(team, amount):
-    tally[team]['totalStatistics']['shotStatistics']['shotsOnGoal'] += amount
-    current_tally = tally[team]['totalStatistics']['shotStatistics']['shotsOnGoal']
+    tally[team]["totalStatistics"]["shotStatistics"]["shotsOnGoal"] += amount
+    current_tally = tally[team]["totalStatistics"]["shotStatistics"]["shotsOnGoal"]
     if current_tally < 0:
-        tally[team]['totalStatistics']['shotStatistics']['shotsOnGoal'] = 0
+        tally[team]["totalStatistics"]["shotStatistics"]["shotsOnGoal"] = 0
         current_tally = 0
-    sio.emit('tally', tally)
+    sio.emit("tally", tally)
     return jsonify(current_tally)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/api')
+@app.route("/api")
 def api():
+    tally["home"] = tally["homeTeamStatistics"]["totalStatistics"]["shotStatistics"][
+        "shotsOnGoal"
+    ]
+    tally["away"] = tally["awayTeamStatistics"]["totalStatistics"]["shotStatistics"][
+        "shotsOnGoal"
+    ]
     return jsonify(tally)
 
 
-@sio.on('connect')
+@sio.on("connect")
 def connect():
-    sio.emit('tally', tally)
+    sio.emit("tally", tally)
 
 
-@sio.on('add')
+@sio.on("add")
 def add(data):
-    modify_tally(data['team'], 1)
+    modify_tally(data["team"], 1)
 
 
-@sio.on('remove')
+@sio.on("remove")
 def remove(data):
-    modify_tally(data['team'], -1)
+    modify_tally(data["team"], -1)
 
 
-@sio.on('reset')
+@sio.on("reset")
 def reset():
-    tally['homeTeamStatistics']['totalStatistics']['shotStatistics']['shotsOnGoal'] = 0
-    tally['awayTeamStatistics']['totalStatistics']['shotStatistics']['shotsOnGoal'] = 0
-    sio.emit('tally', tally)
+    tally["homeTeamStatistics"]["totalStatistics"]["shotStatistics"]["shotsOnGoal"] = 0
+    tally["awayTeamStatistics"]["totalStatistics"]["shotStatistics"]["shotsOnGoal"] = 0
+    sio.emit("tally", tally)
 
 
-if __name__ == '__main__':
-    host = os.environ['HOST'] or 'http://localhost:5000'
-    data = open('templates/index.html', 'r').read()
-    data = data.replace('http://localhost:5000', host)
-    with open('templates/index.html', 'w') as file:
+if __name__ == "__main__":
+    host = os.environ.get("HOST", "http://localhost:5000")
+    data = open("templates/index.html", "r").read()
+    data = data.replace("http://localhost:5000", host)
+    with open("templates/index.html", "w") as file:
         file.write(data)
 
-    sio.run(app, host='0.0.0.0', allow_unsafe_werkzeug=True)
+    sio.run(app, host="0.0.0.0", allow_unsafe_werkzeug=True)
